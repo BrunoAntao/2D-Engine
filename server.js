4e19122395e55b652bd9bad012cf110745713164
Bruno <l37865@alunos.uevora.pt>
2018-09-23 21:51:04 +0100

    added gitignore and node server host

---
+++ server.js
@@ -0,0 +1,17 @@
+const compression = require('compression');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const port = 80;
+
+app.set('view engine', 'ejs');
+app.use(compression());
+app.use('/client', express.static('client'));
+
+require('./server/routes.js')(app);
+
+http.listen(port, function () {
+
+    console.log('listening on: ' + port);
+
+});
