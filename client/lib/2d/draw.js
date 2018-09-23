fb47497388ef23006931add259fea29cfe26066f
brunom76 <brunomantao@gmail.com>
2018-09-23 23:10:22 +0100

    added poly class (work in progress)

--- client/lib/2d/draw.js
+++ client/lib/2d/draw.js
@@ -10,8 +10,6 @@ class Scene {
 
         this.color = color;
 
-        this.i = 0;
-
         this.children = [];
 
         this.draw();
@@ -37,6 +35,45 @@ class Scene {
         }, this)
 
         requestAnimationFrame(()=>this.draw());
+
+    }
+
+}
+
+class Poly {
+
+    constructor (parent, x = 0, y = 0, points = [], fill = '#000000', stroke = '#ffffff') {
+
+        this.parent = parent;
+        this.parent.add(this);
+
+        this.x = x;
+        this.y = y;
+
+        this.points = points;
+
+        this.fill = fill;
+        this.stroke = stroke;
+
+    }
+
+    draw() {
+
+        this.parent.ctx.strokeStyle = this.stroke;
+        this.parent.ctx.fillStyle = this.fill;
+
+        this.parent.ctx.moveTo(this.x, this.y);
+        this.parent.ctx.beginPath();
+
+        this.points.forEach(function(point) {
+
+            this.parent.ctx.lineTo(this.x + point.x, this.y + point.y);
+
+        }, this)
+
+        this.parent.ctx.closePath();
+        this.parent.ctx.fill();
+        this.parent.ctx.stroke();
 
     }
 
